e62074163b2ed9e924f25cb3f4c6c4a2da17abd3
GWC <gwc@GWCs-MacBook-Pro-13.local>
2019-08-14 15:26:36 -0400

    broken code :) sorry

--- gamefile.py
+++ gamefile.py
@@ -4,3 +4,8 @@ pygame.init()
 pygame.mixer.music.load('GameMusic.mp3')
 pygame.mixer.music.play(-1)
 
+#open/close window
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Tutorial 1')
