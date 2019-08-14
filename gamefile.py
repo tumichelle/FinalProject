c9145b832fe4a69ce3be210856a1d33bf90420c9
GWC <gwc@GWCs-MacBook-Pro-13.local>
2019-08-14 15:44:40 -0400

    purple screen and music

--- gamefile.py
+++ gamefile.py
@@ -1,11 +1,22 @@
 import pygame
 
+
 pygame.init()
+# music
 pygame.mixer.music.load('GameMusic.mp3')
 pygame.mixer.music.play(-1)
 
-#open/close window
+background_colour = (120,10,255)
 (width, height) = (300, 200)
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Tutorial 1')
+screen.fill(background_colour)
+
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
