eb2c767af5a6221fa5400e29c0a237890ddf908d
GWC <gwc@GWCs-MacBook-Pro-13.local>
2019-08-15 10:55:10 -0400

    music and intro... ish. it broke

--- gamefile.py
+++ gamefile.py
@@ -2,14 +2,18 @@ import pygame
 
 
 pygame.init()
+
+display_width = 700
+display_height = 391
+black = (0,0,0)
+white = (255, 255, 255)
+background_colour = (0,10,0)
+
 # music
 pygame.mixer.music.load('GameMusic.mp3')
 pygame.mixer.music.play(-1)
 
-background_colour = (0,10,0)
-(width, height) = (300, 200)
-
-screen = pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Escape from: UNEMPLOYMENT')
 screen.fill(background_colour)
 
@@ -20,3 +24,27 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+#intro screen
+
+def text_objects(text, font):
+    textSurface = font.render(text, True,white )
+    return textSurface, textSurface.get_rect()
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        largeText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 10)
+
+        TextSurf, TextRect = text_objects('Escape from Unemployment', largeText)
+        TextRect.center = ((display_width/2),(display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+
+game_intro ()
