ebb6940251afd1c11d4d632ba37ddf1278e348b9
GWC <gwc@GWCs-MacBook-Pro-13.local>
2019-08-16 11:33:38 -0400

    introscreen code

--- introscreen.py
+++ introscreen.py
@@ -12,18 +12,45 @@ red = (255,0,0)
 pygame.mixer.music.load('GameMusic.mp3')
 pygame.mixer.music.play(-1)
 
+largeText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 50)
+mediumText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 35)
+startText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 75)
+unimployedText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 70)
+
+mouse = pygame.mouse.get_pos()
+
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
+
+
 def text_objects(text, font):
-    textSurface = font.render(text, True,white )
+    textSurface = font.render(text, True, white)
     return textSurface, textSurface.get_rect()
 def textStartcolor(text, font):
-    textStart = font.render(text, True, red )
+    textStart = font.render(text, True, red)
     return textStart, textStart.get_rect()
+    
+def button (msg,x,y,w,h, action = None):
+    click = pygame.mouse.get_pressed()
+    print (click)
+    while action== None:
+        if x+w > mouse [0] > x and y+h > mouse [1] > y:
+            TextSurf, TextRect = textStartcolor(msg, mediumText)
+            TextRect.center = ((x),(y))
+            gameDisplay.blit(TextSurf, TextRect)
+            pygame.display.update()
+
+            if click[0] == 1 and action != None:
+                action() 
+        else:
+            TextSurf, TextRect = text_objects(msg, mediumText)
+            TextRect.center = ((x),(y))
+            gameDisplay.blit(TextSurf, TextRect)
+            pygame.display.update()
+
 
 def game_intro():
     intro = True
-
 
     while intro:
         for event in pygame.event.get():
@@ -31,15 +58,9 @@ def game_intro():
                 pygame.quit()
                 quit()
         mouse = pygame.mouse.get_pos()
-        print(mouse)
+
         background_image = pygame.image.load("StartBackground.png")
         gameDisplay.blit(background_image, [0, 0])
-
-        largeText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 50)
-        mediumText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 35)
-        startText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 75)
-        unimployedText = pygame.font.SysFont("DisposableDroidBB copy.ttf", 70)
-
         TextSurf, TextRect = text_objects('ESCAPE FROM', largeText)
         TextRect.center = ((350),(35))
         gameDisplay.blit(TextSurf, TextRect)
@@ -49,43 +70,23 @@ def game_intro():
         TextRect.center = ((345),(180))
         gameDisplay.blit(TextSurf, TextRect)
         pygame.display.update()
-
+        button("Instructions",125,330,190,90)
+        button("About",550,330,190,90)
 #start button
         if 260+180 > mouse [0] > 260 and 305+60 > mouse [1] > 305:
-            pygame.draw.rect(gameDisplay, red, [260, 305, 180,60], 5)
+            pygame.draw.rect(gameDisplay, red, [260, 305, 180, 60], 5)
             TextSurf, TextRect = textStartcolor('START', startText)
             TextRect.center = ((350),(335))
             gameDisplay.blit(TextSurf, TextRect)
             pygame.display.update()
 
         else:
-            pygame.draw.rect(gameDisplay, white, [260, 305, 180,60], 5)
+            pygame.draw.rect(gameDisplay, white, [260, 305, 180, 60], 5)
             TextSurf, TextRect = text_objects('START', startText)
             TextRect.center = ((350),(335))
             gameDisplay.blit(TextSurf, TextRect)
             pygame.display.update()
 
-#about button
-        if 530+190 > mouse [0] > 530 and 320+90 > mouse [1] > 320:
-            TextSurf, TextRect = textStartcolor('About', mediumText)
-            TextRect.center = ((575),(335))
-            gameDisplay.blit(TextSurf, TextRect)
-            pygame.display.update()
-        else:
-            TextSurf, TextRect = text_objects('About', mediumText)
-            TextRect.center = ((575),(335))
-            gameDisplay.blit(TextSurf, TextRect)
-            pygame.display.update()
 
-#Instructions Button
-        if 25+190 > mouse [0] > 25 and 310+90 > mouse [1] > 310:
-            TextSurf, TextRect = textStartcolor('Instructions', mediumText)
-            TextRect.center = ((125),(335))
-            gameDisplay.blit(TextSurf, TextRect)
-            pygame.display.update()
-        else:
-            TextSurf, TextRect = text_objects('Instructions', mediumText)
-            TextRect.center = ((125),(335))
-            gameDisplay.blit(TextSurf, TextRect)
-            pygame.display.update()
-game_intro ()
+game_intro()
+
